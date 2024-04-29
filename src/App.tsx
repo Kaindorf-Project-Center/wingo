2e7ee51a14902c53f0b883723abfc321d17ad3b9
Laurens Kropf <laurens.kropf@kropf-it.at>
2024-04-29 13:38:03 +0200

    fixed initial user get

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,47 @@ import BuildBingo from "@/pages/BuildBingo.tsx";
 import { Dashboard } from "@/pages/Dashboard.tsx";
 import LandingPage from "@/pages/LandingPage.tsx";
 import NotFound from "@/pages/NotFound.tsx";
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { container } from "tsyringe";
 import { useSubscribe } from "./hooks/useSubscribe";
 import AppCache from "./models/AppCache";
+import { backendURL } from "./static";
 
 function App() {
   const appCache = container.resolve(AppCache);
   const userdata = useSubscribe(appCache.userdata);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (appCache.userdata.value == null) {
+      fetch(backendURL + "/user", {
+        mode: "cors",
+        credentials: "include",
+      })
+        .then((data) => data.json())
+        .then((json) => {
+          console.log(appCache.userdata.getValue());
+          appCache.userdata.next({
+            discordId: json.discordId,
+            username: json.username,
+          });
+          console.log(appCache.userdata.getValue());
+
+          // Navigate to dashboard if logged in
+          if (location.pathname == "/") {
+            navigate("/dashboard");
+          }
+        })
+        .catch(() => {
+          if (location.pathname != "/") {
+            navigate("/");
+          }
+        });
+    }
+  }, []);
 
   return (
     <Routes>
--- src/components/wingo/NavbarContainer.tsx
+++ src/components/wingo/NavbarContainer.tsx
@@ -1,32 +1,7 @@
-import AppCache from "@/models/AppCache";
-import { backendURL } from "@/static";
-import { useEffect } from "react";
 import { Outlet } from "react-router";
-import { container } from "tsyringe";
 import { Navbar } from "./Navbar";
 
 const NavbarContainer = () => {
-  const appCache = container.resolve(AppCache);
-
-  useEffect(() => {
-    if (appCache.userdata.value == null) {
-      fetch(backendURL + "/user", {
-        mode: "cors",
-        credentials: "include",
-      })
-        .then((data) => data.json())
-        .then((json) => {
-          console.log(appCache.userdata.getValue());
-          appCache.userdata.next({
-            discordId: json.discordId,
-            username: json.username,
-          });
-          console.log(appCache.userdata.getValue());
-        })
-        .catch(() => window.location.assign("/"));
-    }
-  }, []);
-
   return (
     <div>
       <Navbar />
