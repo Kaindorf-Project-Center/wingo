f142c580ed6acead63e465c641aad04af6613b03
JokoFlex <christopher.haindl06@gmail.com>
2024-04-30 08:14:19 +0200

    changed AppCache back to minimum

--- src/models/AppCache.ts
+++ src/models/AppCache.ts
@@ -4,23 +4,6 @@ import {IUserData} from "./IUserData.ts";
 
 @singleton()
 class AppCache {
-    // Cache to session storage and fetch if present, do alsa a refetch to ensure data integretiy
-    private _userdata: BehaviorSubject<IUserData | null> = new BehaviorSubject<IUserData | null>(null);
-
-    get userdata(): BehaviorSubject<IUserData | null> {
-        return this._userdata;
-    }
-
-    set userdata(value: IUserData | null) {
-        this._userdata.next(value)
-        sessionStorage.setItem("userdata", JSON.stringify(value))
-    }
-
-    constructor() {
-        const userdataSave: string | null = sessionStorage.getItem("userdata")
-        if (userdataSave) {
-            this._userdata = new BehaviorSubject<IUserData | null>(JSON.parse(userdataSave) as (IUserData | null));
-        }
-    }
+    userdata: BehaviorSubject<IUserData | null> = new BehaviorSubject<IUserData | null>(null);
 }
 export default AppCache;
